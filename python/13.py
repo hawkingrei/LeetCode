df361c211747e33020a98d7e39b1780401841ddf
真人 <weizhen.wang@duitang.com>
2017-04-19 16:50:06 +0800

    feat: add 13. Roman to Integer

---
+++ python/13.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def romanToInt(self, s):
+        ROMAN = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        if s == "":
+            return 0
+            
+        index = len(s) - 2
+        sum = ROMAN[s[-1]]
+        while index >= 0:
+            if ROMAN[s[index]] < ROMAN[s[index + 1]]:
+                sum -= ROMAN[s[index]]
+            else:
+                sum += ROMAN[s[index]]
+            index -= 1
+        return sum
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.romanToInt("XX")
