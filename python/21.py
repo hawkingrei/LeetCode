fcf4741712b2b1ec42d4dea1e8e006e4f548a3af
真人 <weizhen.wang@duitang.com>
2017-05-06 18:03:44 +0800

    feat: update 21. Recursive Solution

--- python/21.py
+++ python/21.py
@@ -16,3 +16,13 @@ class Solution(object):
             tmp.next = l2
         return result.next
 
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if not l1 or not l2 : return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+        return l1 if l1.val < l2.val else l2 
