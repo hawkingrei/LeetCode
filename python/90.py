14bf9b22cdc794385221618e25672ab086603beb
真人 <weizhen.wang@duitang.com>
2017-07-19 22:11:02 +0800

    feat: add 90. Subsets II

---
+++ python/90.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        res = [[]]
+        nums.sort()
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
+                l = len(res)
+            for j in range(len(res) - l, len(res)):
+                res.append(res[j] + [nums[i]])
+        return res
+        
