1efd0747e636c7f7806c3539d5a6597e5d47e042
真人 <weizhen.wang@duitang.com>
2017-09-25 12:15:51 +0800

    feat: add golang 561. Array Partition I

---
+++ golang/561/main.go
@@ -0,0 +1,11 @@
+import "fmt"
+import "sort"
+
+func arrayPairSum(nums []int) int {
+    sort.Ints(nums)
+    res := 0
+    for i := 0; i < len(nums); i += 2 {
+        res += nums[i]
+    }
+    return res
+}
