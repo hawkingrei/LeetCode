1e35728ff5b087bbdfc89db5ba87180dab978d62
wangweizhen <wangweizhen@bilibili.com>
2020-02-12 01:52:10 +0800

    no671

--- rust/no671/src/lib.rs
+++ rust/no671/src/lib.rs
@@ -19,26 +19,28 @@ impl TreeNode {
         }
     }
 }
-
 pub fn find_second_minimum_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     let mut rtn = i32::MAX;
-    if let Some(n) = root.clone() {
-        let min = n.borrow().val;
-        traverse(root, min, &mut rtn);
-        return if rtn == i32::MAX { -1 } else { rtn };
+    let mut changed = false;
+
+    if let Some(r) = root.clone() {
+        let min = r.borrow().val;
+        traverse(root, min, &mut rtn,&mut changed);
+        return if changed { rtn } else { -1 };
     }
-    -1
+    return -1;
 }
 
-fn traverse(root: Option<Rc<RefCell<TreeNode>>>, min: i32, rtn: &mut i32) {
+fn traverse(root: Option<Rc<RefCell<TreeNode>>>, min: i32, rtn: &mut i32,is_changed: &mut bool) {
     if let Some(r) = root {
         let val = r.borrow().val;
-        if *rtn > val && val > min {
+        if *rtn >= val && val > min {
             *rtn = val;
+            *is_changed = true;
         }
-
-        traverse(r.borrow().left.clone(), min, rtn);
-        traverse(r.borrow().right.clone(), min, rtn);
+        
+        traverse(r.borrow().left.clone(), min, rtn,is_changed);
+        traverse(r.borrow().right.clone(), min, rtn,is_changed);
     }
     return;
 }
