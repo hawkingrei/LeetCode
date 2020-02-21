d1c8390be282e5f07078754cfd1b0ee9a06cf62c
wangweizhen <wangweizhen@bilibili.com>
2020-02-21 21:59:04 +0800

    update no110

---
+++ rust/no110/src/lib.rs
@@ -0,0 +1,59 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn balanced_helper(root: Option<&Rc<RefCell<TreeNode>>>) -> Option<usize> {
+    if let Some(node) = root {
+        let pair = (
+            balanced_helper(node.borrow().right.as_ref()),
+            balanced_helper(node.borrow().left.as_ref()),
+        );
+        match pair {
+            (Some(right), Some(left)) => {
+                if right > left {
+                    if right - left <= 1 {
+                        return Some(right + 1);
+                    }
+                    return None;
+                } else {
+                    if left - right <= 1 {
+                        return Some(left + 1);
+                    }
+                    return None;
+                }
+            }
+            _ => return None,
+        }
+    } else {
+        return Some(0);
+    }
+}
+
+pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    balanced_helper(root.as_ref()).is_some()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
