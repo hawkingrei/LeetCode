449b87bde6d0397afae47c47abf04894daac207f
wangweizhen <wangweizhen@bilibili.com>
2020-04-01 00:31:25 +0800

    154

---
+++ python/154.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        low = 0
+        high = len(nums) - 1
+        while right > left:
+            pivot = low + (high - low) // 2
+            if nums[pivot] < nums[high]:
+                high = pivot
+                # alternative: high = pivot - 1
+                # too aggressive to move the `high` index,
+                # it won't work for the test case of [3, 1, 3]
+            # Case 2):
+            elif nums[pivot] > nums[high]:
+                low = pivot + 1
+            # Case 3):
+            else:
+                high -= 1
+        # the 'low' and 'high' index converge to the inflection point.
+        return nums[low]
