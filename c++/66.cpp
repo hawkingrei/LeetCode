39b7398c7ab7725775fff330702c45f1dde54097
wangweizhen <wangweizhen@bilibili.com>
2021-03-14 15:21:13 +0800

    66

---
+++ c++/66.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int index = digits.size()-1;index >= 0; index++) {
+            digits[index] += 1;
+            digits[index] = digits[index] % 10;
+            if (digits[index] != 0) {
+                return digits;
+            }
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
