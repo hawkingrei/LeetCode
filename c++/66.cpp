cbc7d9a3b9065e1d61edd9cc02e513d6605e8795
wangweizhen <wangweizhen@bilibili.com>
2021-03-14 15:24:00 +0800

    66

--- c++/66.cpp
+++ c++/66.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for (int index = digits.size()-1;index >= 0; index++) {
+        for (int index = digits.size()-1;index >= 0; index--) {
             digits[index] += 1;
             digits[index] = digits[index] % 10;
             if (digits[index] != 0) {
