58cc2b7b1792de98b7e5493d16078e3ccd78d4dc
wangweizhen <wangweizhen@bilibili.com>
2021-04-04 17:46:30 +0800

    feat: update 221

--- c++/221.cpp
+++ c++/221.cpp
@@ -23,23 +23,3 @@ public:
         return maxSquare;
     }
 };
-
-class Solution {
-public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (matrix[i][j] == '1') {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-                    }
-                    maxSide = max(maxSide, dp[i][j]);
-                }
-            }
-        }
-
-    }
-};
